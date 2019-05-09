bd109b5c24dcf65ba8c45f0320caeea8b66eb674
Akshaya Kumar T <t.akshaya.kumar17@gmail.com>
2019-05-09 22:32:16 +0530

    context menu added for play next

--- background.js
+++ background.js
@@ -4,3 +4,19 @@ chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab) {
         chrome.tabs.sendMessage(parseInt(tab.id), {message:"skip_ad",script:"background.js",tab:tab.tabId});
     }
 })
+
+var contextMenuItem = {
+    "id": "videoLink",
+    "title": "Play Next",
+    "contexts" : ["link"],
+    "targetUrlPatterns": ["https://www.youtube.com/watch?*"],
+    "documentUrlPatterns": ["https://www.youtube.com/*"]
+};
+
+chrome.contextMenus.create(contextMenuItem);
+
+// chrome.contextMenus.onClicked.addListener(function(clickData, tab) {
+//     if(clickData.menuItemId == "videoLink" && clickData.linkUrl) {
+//         chrome.tabs.sendMessage(parseInt(tab),{message:"playNext",videoUrl: clickData.linkUrl,tab:tab.tabId});
+//     }
+// })
